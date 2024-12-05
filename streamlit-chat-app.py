14bd9fd50961c56ffc7cb0f4e1fd12eb81b9a96a
Niharika0306 <Niharika.Gurram1@ibm.com>
2024-12-05 11:30:41 +0530

    chat application script

---
+++ streamlit-chat-app.py
@@ -0,0 +1,43 @@
+from openai import OpenAI
+import streamlit as st
+
+st.title("vLLM on Power - ChatBot")
+
+# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+# if "openai_model" not in st.session_state:
+#     st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="meta-llama/Llama-3.1-8B-Instruct",
+            temperature=0.2,
+            max_tokens=30,
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
